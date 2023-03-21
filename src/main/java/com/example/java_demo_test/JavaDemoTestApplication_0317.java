f412e45931dc27160e940e9f8a535caa6a83e807
TONYFU\WIN10 <tonyfu13@gmail.com>
2023-03-21 09:09:57 +0800

    demo_test_0321

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaDemoTestApplication {
+public class JavaDemoTestApplication_0317 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaDemoTestApplication.class, args);
+		SpringApplication.run(JavaDemoTestApplication_0317.class, args);
 	}
 
 }
