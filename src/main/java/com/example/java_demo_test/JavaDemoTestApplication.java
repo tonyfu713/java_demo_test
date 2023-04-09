75428d4978f120b765ba75734ed091c1d3e85b47
TONYFU\WIN10 <tonyfu13@gmail.com>
2023-04-09 21:57:08 +0800

    Ha123

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaDemoTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow Word");
+		System.out.println("Hellow 123");
 		SpringApplication.run(JavaDemoTestApplication.class, args);
 	}
 
